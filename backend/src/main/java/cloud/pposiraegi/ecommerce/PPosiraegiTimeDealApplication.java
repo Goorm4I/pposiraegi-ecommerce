2422857146b4f2d943b15508945199284b911b39
IriyaSE <wndnjs9289@naver.com>
2026-02-26 14:03:14 +0900

    chore: project initial setup with spring boot and postgresql
    - Spring Boot 3.x 프로젝트 초기화
    - PostgreSQL 데이터베이스 연결 설정 (dev, prod, test 프로파일 분리)

---
+++ backend/src/main/java/cloud/pposiraegi/ecommerce/PPosiraegiTimeDealApplication.java
@@ -0,0 +1,13 @@
+package cloud.pposiraegi.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PPosiraegiTimeDealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PPosiraegiTimeDealApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/cloud/pposiraegi/ecommerce/PPosiraegiTimeDealApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.pposiraegi.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PPosiraegiTimeDealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
