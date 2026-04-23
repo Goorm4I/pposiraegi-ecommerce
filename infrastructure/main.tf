9a7fe65bbe8374a1dcdc07137a9873ad9b4bcd84
Lee-nahyung <skgud0125@naver.com>
2026-04-23 09:40:44 +0900

    refactor: terraform 모듈화 적용 (networking, security, storage 모듈 분리)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -29,7 +29,6 @@ terraform {
 # Data
 ###############################################################
 data "aws_availability_zones" "available" {}
-data "aws_caller_identity" "current" {}
 
 ###############################################################
 # Modules
