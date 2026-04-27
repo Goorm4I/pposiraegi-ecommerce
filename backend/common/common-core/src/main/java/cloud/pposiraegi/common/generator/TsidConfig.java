346b4d1094193564f458ad1e5b97eeb36fe5cec6
iriyaSE <wndnjs9289@naver.com>
2026-04-27 10:41:02 +0900

    Feat: Tsid 노드 설정 추가

--- backend/common/common-core/src/main/java/cloud/pposiraegi/common/generator/TsidConfig.java
+++ backend/common/common-core/src/main/java/cloud/pposiraegi/common/generator/TsidConfig.java
@@ -1,14 +1,27 @@
 package cloud.pposiraegi.common.generator;
 
 import com.github.f4b6a3.tsid.TsidFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.InetAddress;
+
 @Configuration
 public class TsidConfig {
+    @Value("{NODE_ID:0.0.0.0}")
+    private String podIP;
+
     @Bean
     public TsidFactory tsidFactory() {
-        int nodeId = 0;
+        int nodeId;
+        try {
+            InetAddress ip = InetAddress.getByName(podIP);
+            byte[] bytes = ip.getAddress();
+            nodeId = ((bytes[2] & 0x03) << 8) | (bytes[3] & 0xFF);
+        } catch (Exception e) {
+            nodeId = 0;
+        }
         return TsidFactory.builder()
                 .withNode(nodeId)
                 .build();
