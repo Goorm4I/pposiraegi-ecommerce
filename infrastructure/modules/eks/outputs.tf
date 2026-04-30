7a38132d67b2413221c30574942272ffdc14d4b2
Lee-nahyung <skgud0125@naver.com>
2026-04-30 14:58:02 +0900

    fix : module/eks/outputs.tf 중복 cluster_security_group_id 삭제

--- infrastructure/modules/eks/outputs.tf
+++ infrastructure/modules/eks/outputs.tf
@@ -22,7 +22,3 @@ output "node_group_arn" {
   description = "EKS 노드 그룹 ARN"
   value       = aws_eks_node_group.main.arn
 }
-
-output "cluster_security_group_id" {
-  value = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
-}
