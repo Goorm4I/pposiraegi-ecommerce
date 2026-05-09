13d3c6ea82fce1b9bc424519165356a6310fa9c4
Lee-nahyung <skgud0125@naver.com>
2026-05-10 07:20:47 +0900

    fix: re-add jihoon to eks_admin_principal_arns
    - jihoon kubectl 접근을 위해 eks_admin_principal_arns에 재추가

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -130,6 +130,7 @@ variable "eks_admin_principal_arns" {
   description = "EKS cluster admin access entries for team operators"
   type        = list(string)
   default = [
+    "arn:aws:iam::779846782353:user/jihoon",
     "arn:aws:iam::779846782353:user/nahyung",
   ]
 }
